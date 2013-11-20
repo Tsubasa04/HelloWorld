8b343d5c7f8c363c3014c4b73ef064198faa721e
Tsubasa SHINZATO <e125704@Tsubasa-no-MacBook-Pro.local>
2013-11-21 01:07:42 +0900

    Initial Commit

---
+++ HelloWorld/HelloAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HelloAppDelegate.h
+//  HelloWorld
+//
+//  Created by Tsubasa SHINZATO on 2013/11/21.
+//  Copyright (c) 2013年 Tsubasa SHINZATO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HelloAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HelloWorld/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HelloWorld
+//
+//  Created by Tsubasa SHINZATO on 2013/11/21.
+//  Copyright (c) 2013年 Tsubasa SHINZATO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HelloAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HelloAppDelegate class]));
+    }
+}
